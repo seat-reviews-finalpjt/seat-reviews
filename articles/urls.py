f0db67b28a1d3cb3cc697152f6dc7a03e1764987
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-14 16:51:34 +0900

    fix: comment urls 작성 & 댓글 생성 APIView 작성

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import CommentListAPIView
 
 urlpatterns = [
-
+    path("<int:article_pk>/", CommentListAPIView.as_view(), name="comment_list"),
+    path("<int:article_pk>/comments/",
+         CommentListAPIView.as_view(), name="comment_list"),
 ]
