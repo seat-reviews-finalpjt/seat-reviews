bc65ec100a403a7a4637e86878996f36cf846ab1
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-14 17:15:44 +0900

    fix: 댓글 수정 삭제 views 작성

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import CommentListAPIView
+from .views import CommentListAPIView, CommentDetailAPIView
 
 urlpatterns = [
     path("<int:article_pk>/", CommentListAPIView.as_view(), name="comment_list"),
     path("<int:article_pk>/comments/",
          CommentListAPIView.as_view(), name="comment_list"),
-]
+    path("<int:article_pk>/comments/<int:comment_pk>",
+         CommentDetailAPIView.as_view(), name="comment_list"),
+]  # 글 CRUD 과정에서 URL 수정 필수!
--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,7 @@ from .serializers import CommentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
 
 
 class CommentListAPIView(APIView):
@@ -17,3 +18,34 @@ class CommentListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailAPIView(APIView):
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        self.check_object_permissions(request, comment)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        comment = self.get_object(pk)
+        self.check_object_permissions(request, comment)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
