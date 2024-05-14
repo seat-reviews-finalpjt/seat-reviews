21d50e87634816235c16a817f0c1cc8726fc1ec4
Yosi <lyt2006@naver.com>
2024-05-14 17:38:28 +0900

    feat: 회원가입 기능 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    GENDER_CHOICES = {
+        ("male", "남성"),
+        ('female','여성'),
+        ("order", "또 다른 성"),
+    }
+    nickname=models.CharField(max_length=20, unique = True)
+    name=models.CharField(max_length=10)
+    gender=models.CharField(choices=GENDER_CHOICES, blank=True)
+    birthday=models.DateField(auto_now=False, auto_now_add=False, blank=True)
+    
+    
+    def __str__(self):
+        return self.username
