a28cbaed0fae7e3860105da630ec001ccc76ddc9
leejinwon012 <leejinwon012@naver.com>
2024-05-14 18:31:54 +0900

    feat: 게시글 목록조회 + 게시글 생성 구현 완료(권한 설정 임시로 열어놓음)

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import ArticleList
 
 urlpatterns = [
-
+    path('', ArticleList.as_view(), name='article-list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [permissions.AllowAny] # permissions.IsAuthenticated 테스트 해보려고 아무나 가능하도록 해놨음
+        else:
+            self.permission_classes = [permissions.AllowAny]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
