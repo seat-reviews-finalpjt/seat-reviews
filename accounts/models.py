e7f56e07623d65e8486a03b7b586ae7c8869b9bb
Yosi <lyt2006@naver.com>
2024-05-14 18:58:42 +0900

    fix: 마이그레이션 오류 수정

--- accounts/models.py
+++ accounts/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    GENDER_CHOICES = {
+    GENDER_CHOICES = [
         ("male", "남성"),
-        ('female','여성'),
-        ("order", "또 다른 성"),
-    }
+        ('female', '여성'),
+        ("other", "또 다른 성"),
+    ]
     nickname=models.CharField(max_length=20, unique = True)
     name=models.CharField(max_length=10)
-    gender=models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
-    birthday=models.DateField(auto_now=False, auto_now_add=False, blank=True)
+    gender=models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    birthday=models.DateField(null=True, blank=True)
     
     
     def __str__(self):
