b60745f5d27245284ecf67a1e2777e6325ad3b88
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-16 10:53:12 +0900

    fix: comment model, serializers 수정

--- articles/serializers.py
+++ articles/serializers.py
@@ -5,8 +5,8 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'article_id',
-                  'commenter_id', 'created_at', 'updated_at']
+        fields = ['id', 'content', 'article',
+                  'commenter', 'created_at', 'updated_at']
 
     def get_replies(self, obj):
         replies = Comment.objects.filter(parent_comment=obj)
