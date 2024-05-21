38353d8ac67f3fbff4da72dd38ae5e35bfdd9075
Yosi <lyt2006@naver.com>
2024-05-21 21:06:47 +0900

    feat: search 기능 추가. ai 추천 기능은 불완정

--- searches/models.py
+++ searches/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from accounts.models import User
 
 class SearchHistory(models.Model):
     query = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-
-# search/serializers.py
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- searches/views.py
+++ searches/views.py
@@ -5,8 +5,11 @@ from articles.serializers import ArticleSerializer
 from .models import SearchHistory
 from .serializers import SearchHistorySerializer
 from transformers import pipeline
+from rest_framework.permissions import IsAuthenticated
 
 class SearchView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         query = request.GET.get('q', '')
         
@@ -14,13 +17,30 @@ class SearchView(APIView):
         articles = Article.objects.filter(title__icontains=query)
         serializer = ArticleSerializer(articles, many=True)
         
+        # 인증된 사용자인지 확인
+        user = request.user
+        
         # 검색 기록을 데이터베이스에 저장
-        search_history = SearchHistory(query=query)
+        search_history = SearchHistory(query=query, user=user)
         search_history.save()
         
-        # Hugging Face Transformers를 사용하여 사용자 추천 알고리즘 구현
-        text_generator = pipeline('text-generation', model='gpt2')
-        recommendations = text_generator(f"Based on the search query '{query}', here are some movie recommendations:", max_length=100, num_return_sequences=3)
-        
-        return Response({'articles': serializer.data, 'recommendations': recommendations})
+        return Response({'articles': serializer.data})
+    
+# class RecommendationView(APIView):
+#     permission_classes = [IsAuthenticated]
 
+#     def get(self, request):
+#         user = request.user
+#         search_histories = SearchHistory.objects.filter(user=user).values_list('query', flat=True)
+        
+#         if search_histories:
+#             queries = ' '.join(search_histories)
+            
+#             # Hugging Face Transformers를 사용하여 사용자 추천 알고리즘 구현
+#             text_generator = pipeline('text-generation', model='gpt2')
+#             prompt = f"'{queries}', 이 데이터를 알고리즘으로, 이 유저가 좋아할 만한 영화를 3개 추천해줘. 한국어로 제목만 줬으면 좋겠어."
+#             recommendations = text_generator(prompt, max_new_tokens=100, num_return_sequences=3)
+            
+#             return Response({'recommendations': recommendations})
+#         else:
+#             return Response({'recommendations': []})
