3be7aef63b34367b828e6a0e0280e6ea7c0fad13
Yosi <lyt2006@naver.com>
2024-05-23 10:33:35 +0900

    fix: 사진 업로드 수정 및 ai 추천 기능 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name= 'accounts'
 
