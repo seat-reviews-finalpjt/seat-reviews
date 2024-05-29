72c43b7d29c741ef1be2ceab299c8a75403998aa
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-29 19:51:16 +0900

    feat: review 모델/시리얼라이저 작성

--- articles/models.py
+++ articles/models.py
@@ -4,53 +4,53 @@ from django.conf import settings
 from rest_framework import permissions
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='photos/')
-    description = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # User 모델 참조
-    created_at = models.DateTimeField(auto_now_add=True)
+# class Article(models.Model):
+#     title = models.CharField(max_length=255)
+#     photo = models.ImageField(upload_to='photos/')
+#     description = models.TextField()
+#     author = models.ForeignKey(
+#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # User 모델 참조
+#     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
-    @property
-    def author_username(self):
-        return self.author.username
-
-
-class ArticlesLike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    liked_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'article')
+#     @property
+#     def author_username(self):
+#         return self.author.username
 
 
-class Comment(models.Model):
-    content = models.CharField(max_length=100)
-    article = models.ForeignKey(
-        Article, on_delete=models.CASCADE)
-    commenter = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    parent_comment = models.ForeignKey(
-        'self', on_delete=models.CASCADE, blank=True, null=True, related_name='replies')
+# class ArticlesLike(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     liked_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.content
+#     class Meta:
+#         unique_together = ('user', 'article')
 
 
-class CommentLike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    liked_at = models.DateTimeField(auto_now_add=True)
+# class Comment(models.Model):
+#     content = models.CharField(max_length=100)
+#     article = models.ForeignKey(
+#         Article, on_delete=models.CASCADE)
+#     commenter = models.ForeignKey(
+#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     parent_comment = models.ForeignKey(
+#         'self', on_delete=models.CASCADE, blank=True, null=True, related_name='replies')
 
-    class Meta:
-        unique_together = ('user', 'comment')  # 중복 방지
+#     def __str__(self):
+#         return self.content
 
+
+# class CommentLike(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+#     liked_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         unique_together = ('user', 'comment')  # 중복 방지
 
 
 class Theater(models.Model):
@@ -63,7 +63,8 @@ class Theater(models.Model):
 
 
 class Seat(models.Model):
-    theater = models.ForeignKey(Theater, related_name='seats', on_delete=models.CASCADE)
+    theater = models.ForeignKey(
+        Theater, related_name='seats', on_delete=models.CASCADE)
     row = models.CharField(max_length=10)
     number = models.IntegerField()
     x_position = models.FloatField()
@@ -74,3 +75,34 @@ class Seat(models.Model):
 
     def __str__(self):
         return f"{self.theater.name} - Row {self.row}, Seat {self.number}"
+
+# 05.29 오전 회의 상황 반영 사용 / 확인 필요
+
+
+class Review(models.Model):
+    SCORE_CHOICES = [
+        (1, '1점'),
+        (2, '2점'),
+        (3, '3점'),
+        (4, '4점'),
+        (5, '5점'),
+    ]
+    photo = models.ImageField(upload_to='photos/')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # User 모델 참조
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=500)
+    score = models.IntegerField(choices=SCORE_CHOICES)
+
+    def __str__(self):
+        return self.content
+
+
+class ReviewLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'review')
--- articles/serializers.py
+++ articles/serializers.py
@@ -1,27 +1,26 @@
-from .models import Theater, Seat
+from .models import Theater, Seat, Review
 from rest_framework import serializers, viewsets
 from rest_framework import serializers
-from .models import Article, Comment
+# from .models import Article, Comment,
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-    author_username = serializers.ReadOnlyField(source='author.username')
+# class ArticleSerializer(serializers.ModelSerializer):
+#     author_username = serializers.ReadOnlyField(source='author.username')
 
-    class Meta:
-        model = Article
-        fields = ['id', 'title', 'photo', 'description', 'author_username', 'created_at']
+#     class Meta:
+#         model = Article
+#         fields = ['id', 'title', 'photo', 'description', 'author_username', 'created_at']
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['id', 'article', 'commenter', 'content','created_at', 'updated_at', 'parent_comment']
-        read_only_fields = ['commenter', 'article']
+# class CommentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Comment
+#         fields = ['id', 'article', 'commenter', 'content','created_at', 'updated_at', 'parent_comment']
+#         read_only_fields = ['commenter', 'article']
 
-    def get_replies(self, obj):
-        replies = Comment.objects.filter(parent_comment=obj)
-        return CommentSerializer(replies, many=True).data
-
+#     def get_replies(self, obj):
+#         replies = Comment.objects.filter(parent_comment=obj)
+#         return CommentSerializer(replies, many=True).data
 
 
 class TheaterSerializer(serializers.ModelSerializer):
@@ -38,7 +37,9 @@ class TheaterViewSet(viewsets.ModelViewSet):
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seat
-        fields = ['id', 'row', 'number', 'x_position', 'y_position', 'is_available', 'width', 'height']
+        fields = ['id', 'row', 'number', 'x_position',
+                  'y_position', 'is_available', 'width', 'height']
+
 
 class TheaterSerializer(serializers.ModelSerializer):
     seats = SeatSerializer(many=True, read_only=True)
@@ -46,3 +47,12 @@ class TheaterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Theater
         fields = ['id', 'name', 'location', 'description', 'seats']
+
+# 05.29 오전 회의 상황 반영 사용 / 확인 필요
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'photo', 'author', 'content',
+                  'created_at', 'updated_at', 'score']
