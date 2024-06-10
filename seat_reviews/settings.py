930328b66a0e663fb2325a72000a555a360c1ede
Yosi <lyt2006@naver.com>
2024-06-10 20:03:20 +0900

    fix: settings 파일 수정

--- seat_reviews/settings.py
+++ seat_reviews/settings.py
@@ -97,10 +97,9 @@ CHANNEL_LAYERS = {
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
