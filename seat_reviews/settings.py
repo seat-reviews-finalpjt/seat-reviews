4512ab0d4c01cffc066f587dd80857a58778b010
Yosi <lyt2006@naver.com>
2024-06-13 01:50:03 +0900

    fix: postgre fix

--- seat_reviews/settings.py
+++ seat_reviews/settings.py
@@ -90,19 +90,19 @@ CHANNEL_LAYERS = {
     },
 }
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL')
-#     )
-# }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 AUTH_USER_MODEL = 'accounts.User'
 
